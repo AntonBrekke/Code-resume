05446711c7338642c532e3037af60a32762edbf5
AntonBrekke <anton.a@hotmail.no>
2023-11-05 23:20:31 +0100

    Added files

--- Python/Single Python-files/monte_carlo_integral_parallell.py
+++ Python/Single Python-files/monte_carlo_integral_parallell.py
@@ -4,16 +4,21 @@ from numba import njit
 import time
 import multiprocessing as mp
 
+"""
+Really simple example of Monte-Carlo integral
+with paralellized code.
+"""
+
 a = 0
 b = np.pi
-N = 25000
-M = 100000
+N = 25000       # Number of points
+M = 100000      # Number of Monte-Carlo cycles
 
 t0 = time.time()
 
 @njit
 def f(x):
-    return np.sin(x)
+    return np.sin(x)        # True integral value of this on 0, pi is 2 
 
 @njit(cache=True)
 def monte_carlo(repeat_dummy):
@@ -26,26 +31,26 @@ def monte_carlo(repeat_dummy):
 if __name__ == '__main__':
     t0 = time.time()
 
-    proc_num = 10
+    proc_num = 6
     with mp.Pool(proc_num) as pool:
-        mc = pool.map(monte_carlo, np.ones(M))
+        mc = pool.map(monte_carlo, np.ones(M))      # Running on 6 threads repeating M times
 
     mc = np.array(mc)
     t1 = time.time()
     print(f'Computation time {t1 - t0}s')
 
-    # Lager en figur som skal se kulere ut
+    # Making figure of data
     fig = plt.figure(facecolor='k')
     ax = fig.add_subplot(facecolor='k')
-    ax.spines['bottom'].set_color('w')      # Setter aksen hvit
+    ax.spines['bottom'].set_color('w')      # Setting axis white
     ax.spines['left'].set_color('w')
-    ax.tick_params(axis='x', colors='w')    # Setter ticks hvit
+    ax.tick_params(axis='x', colors='w')    # Setting ticks white
     ax.tick_params(axis='y', colors='w')
 
-    # Henter fargekart for histogram
+    # Getting color-chart for histogram
     get_cmap = plt.get_cmap('jet')
     n, bins, patches = ax.hist(mc, bins=75)
-    cmap = np.array(get_cmap(n / np.max(n)))    # Må normalisere data
+    cmap = np.array(get_cmap(n / np.max(n)))    # Must normalize data
 
     for color, p in zip(cmap, patches):
         plt.setp(p, 'facecolor', color)
